b4ff25cb11febeb5ce74c86497385d90d90b27e7
Saurab <saurajain@deloitte.com>
2019-05-15 09:19:01 -0400

    Added code for Chapter 3

--- src/main/scala/com/techidiocy/spark/examples/sdg/Chapter3.scala
+++ src/main/scala/com/techidiocy/spark/examples/sdg/Chapter3.scala
@@ -1,3 +1,22 @@
 # Chapter 3 - Structured Streaming
 
 # Create a static data frame
+val staticDataFrame = spark.read.format("csv").option("header","true").option("inferSchema", "true").load("c:\\saurav\\git-repo\\Spark-The-Definitive-Guide\\data\\retail-data\\by-day\\*.csv")
+
+# Create a temp view out of the data frame.
+staticDataFrame.createOrReplaceTempView("static_data")
+
+# Get the schema out of the dataframe.
+val staticSchema = staticDataFrame.schema
+
+import org.apache.spark.sql.functions.{window, column, desc, col}
+
+staticDataFrame.selectExpr("CustomerId","(UnitPrice * Quantity) as total_cost", "InvoiceDate")
+.groupBy( col("CustomerId"), window(col("InvoiceDate"), "1 day"))
+.sum("total_cost").show(5)
+
+// spark.sql(""" select CustomerId, (UnitPrice * Quantity) as total_cost, InvoiceDate from static_data group by CustomerId """)
+
+spark.conf.set("spark.sql.shuffle.partitions", "5")
+
+val streamingDataFrame = spark.readStream.format("csv").schema(staticSchema).option("header","true").option("maxFilesPerTrigger",1).load("c:\\saurav\\git-repo\\Spark-The-Definitive-Guide\\data\\retail-data\\by-day\\*.csv")

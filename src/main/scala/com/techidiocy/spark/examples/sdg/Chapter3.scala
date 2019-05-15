d0cd4b4760eef0150c339274c27d30f8ee79fbd5
saurabh.jain <saurabhjain@Saurabhs-MacBook-Pro.local>
2019-05-14 23:35:48 -0400

    Added code for Chapter3 of SDG

--- src/main/scala/com/techidiocy/spark/examples/sdg/Chapter3.scala
+++ src/main/scala/com/techidiocy/spark/examples/sdg/Chapter3.scala
@@ -1,3 +1,10 @@
 # Chapter 3 - Structured Streaming
 
-# Create a static data frame
+# Create a static data frame from the retail data
+val staticDataFrame = spark.read.format("csv").option("inferSchema","true").option("header","true").load("../customdata/Spark-The-Definitive-Guide/data/retail-data/by-day/*.csv")
+
+# Create a temp view or table.
+staticDataFrame.createOrReplaceTempView("retail_data")
+
+# Get the schema out of the table or view.
+val staticSchema = staticDataFrame.schema

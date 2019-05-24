799b7312d57994a2e613da97074ba8888687634e
Saurab <saurajain@deloitte.com>
2019-05-23 20:45:34 -0400

    Added source code for chapter 5 of SDG

--- src/main/scala/com/techidiocy/spark/examples/sdg/Chapter5.scala
+++ src/main/scala/com/techidiocy/spark/examples/sdg/Chapter5.scala
@@ -12,6 +12,46 @@ spark.read.json("C:\\saurav\\git-repo\\Spark-The-Definitive-Guide\\data\\flight-
 
 
 // org.apache.spark.sql.types.StructType = ...
-// StructType(StructField(DEST_COUNTRY_NAME,StringType,true),
-// StructField(ORIGIN_COUNTRY_NAME,StringType,true),
-// StructField(count,LongType,true))
+// StructType(
+//				StructField(DEST_COUNTRY_NAME,StringType,true),
+// 			    StructField(ORIGIN_COUNTRY_NAME,StringType,true),
+// 			    StructField(count,LongType,true)
+//			)
+
+// You can select , manipulate , remove columns from dataframes and these operations are knows as Expressions.
+
+// Get the first record out of the data frame.
+df.first
+
+// Create a row
+import org.apache.spark.sql.Row
+val myRow = Row("Saurabh","Jain",32)
+
+// DataFrame Operations
+//1. Add rows or columns to a DataFrame.
+//2. Remove rows or columns from a DataFrame.
+//3. Transform a row into a column or vice-versa.
+//4. Sort the data by values in rows.
+
+// Create a DataFrame Manually.
+import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType}
+
+// Create schema
+val mySchema = new StructType(Array(new StructField("firstName",StringType, true),
+									new StructField("lastName", StringType, true),
+									new StructField("age", IntegerType, true)))
+
+// Create data
+val rows = Seq(Row("Saurabh", "Jain", 32), Row("Adam", "G", 45))
+
+// Create RDD
+val personRDD = spark.sparkContext.parallelize(rows)
+
+// Create DataFrame from RDD
+val personDF = spark.createDataFrame(personRDD, mySchema)
+personDF.show()
+
+//Create DF from the sequence.
+val seqs = Seq(("Saurabh", "Jain", 32), ("Adam", "G", 45))
+val personDF2 = seqs.toDF()
+personDF2.show()

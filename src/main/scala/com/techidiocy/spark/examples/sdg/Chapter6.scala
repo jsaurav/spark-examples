6a7c41093a13059669a6ae6d7191663c99eb24c9
saurabh.jain <saurabhjain@Saurabhs-MacBook-Pro.local>
2019-05-27 20:40:02 -0400

    Added code for chapter 6 SDG

--- src/main/scala/com/techidiocy/spark/examples/sdg/Chapter6.scala
+++ src/main/scala/com/techidiocy/spark/examples/sdg/Chapter6.scala
@@ -15,10 +15,7 @@
 // lit function - this function converts a type in language to its equivalent spark representation.
 
 // Let's create a dataframe.
-val df = spark.read.format("csv")
-.option("inferSchema","true")
-.option("header","true")
-.load("/Users/saurabhjain/soft/spark-2.3.2-bin-hadoop2.7/customdata/Spark-The-Definitive-Guide/data/retail-data/by-day/2010-12-01.csv")
+val df = spark.read.format("csv").option("inferSchema","true").option("header","true").load("/Users/saurabhjain/soft/spark-2.3.2-bin-hadoop2.7/customdata/Spark-The-Definitive-Guide/data/retail-data/by-day/2010-12-01.csv")
 
 // Create a table
 df.createOrReplaceTempView("dfTable")
@@ -31,8 +28,10 @@ df.where("InvoiceNo < 10000").show(5)
 // In spark you should always chain together "and" filter as sequential filter.
 // or statements need to be specified in the same statement.
 
+// filter and where can be used interchangeably
+
 // Let's create filters.
-val priceFilter = col("UnitPrice") > 10
+val priceFilter = col("UnitPrice") > 600
 val descFilter = col("Description").contains("WHITE")
 
 df.where(col("StockCode").isin("DOT")).where(priceFilter.or(descFilter)).show(5)
@@ -45,3 +44,20 @@ df.where(col("StockCode").isin("DOT")).where(priceFilter).where(descFilter)
 // or statements need to be specified in the same statement.
 
 // To filter a data frame a boolean column can also be used.
+
+val dotCodeFilter = col("StockCode") === "DOT"
+val ffV1 = df.withColumn("is_expensive", dotCodeFilter.and(priceFilter.or(descFilter)))
+.filter(expr("is_expensive"))
+.select("*")
+
+
+// Number Data Types
+import org.apache.spark.sql.functions.POWER
+
+val ffC1 = df.selectExpr("InvoiceNo","StockCode", "(pow((Quantity * UnitPrice),2) + 5) as RealQuantity", "Quantity")
+ffC1.show(5)
+
+// Perason Correlation Coefficient can also be checked.
+
+// What is approx quantile ?
+

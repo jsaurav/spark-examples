8221a362bb619ce7eb85b3e501a97cb179cd3c22
saurabh.jain <saurabhjain@Saurabhs-MacBook-Pro.local>
2019-07-22 18:55:03 -0400

    Added docs for chapter 16 and modified for chapter 7.

---
+++ src/main/scala/com/techidiocy/spark/examples/sdg/Chapter16.scala
@@ -0,0 +1,3 @@
+// Chapter 16 Notes
+
+// Spark applications are the combination of 2 things - Spark Cluster and Spark Code
---
+++ src/main/scala/com/techidiocy/spark/examples/sdg/Chapter7.scala
@@ -0,0 +1,4 @@
+// Chapter 7 Notes
+
+// Aggregating is the act of collecting something together.
+// In aggregation you will provide a key / grouping and an aggregation function.
